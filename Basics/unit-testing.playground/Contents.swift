76803e2856637e02a399df0bab8eed23304ff423
Nick Eddington <neddington@cascadia.edu>
2023-04-28 11:29:06 -0700

    Add logging to testApplyingCouponToCart() function and modify ShoppingCart class to allow access to products array
    This commit adds logging to the testApplyingCouponToCart() function to show the contents of the shopping cart before and after a coupon is applied, as well as the total price. It also modifies the ShoppingCart class to allow access to the products array via a new getProducts() method.
    
    The following changes were made:
    - Added print statements to testApplyingCouponToCart() function to log shopping cart contents and total price
    - Modified ShoppingCart class to add getProducts() method, which returns a copy of the products array

--- Basics/unit-testing.playground/Contents.swift
+++ Basics/unit-testing.playground/Contents.swift
@@ -1,8 +1,3 @@
-// Swift by Sundell sample code
-// https://www.swiftbysundell.com/basics/unit-testing
-// (c) 2019 John Sundell
-// Licensed under the MIT license
-
 import XCTest
 
 // --- Testing mutating a model ---
@@ -31,20 +26,15 @@ class ProductTests: XCTestCase {
 // --- Using a test case's setUp method ---
 
 class ShoppingCartTests: XCTestCase {
-    // Normally, it can be argued that force unwrapping (!) should
-    // be avoided, but in unit tests it can be a good idea for
-    // properties (only!) in order to avoid unnecessary boilerplate.
     private var shoppingCart: ShoppingCart!
 
     override func setUp() {
         super.setUp()
-        // Using this, a new instance of ShoppingCart will be created
-        // before each test is run.
         shoppingCart = ShoppingCart()
     }
 
     func testCalculatingTotalPrice() {
-        // Given: Here we assert that our initial state is correct
+        // Given
         XCTAssertEqual(shoppingCart.totalPrice, 0)
 
         // When
@@ -54,9 +44,71 @@ class ShoppingCartTests: XCTestCase {
         // Then
         XCTAssertEqual(shoppingCart.totalPrice, 35)
     }
+
+    func testApplyingCouponToCart() {
+        // Given
+        shoppingCart.add(Product(name: "Book", price: 20))
+        shoppingCart.add(Product(name: "Movie", price: 15))
+        let coupon = Coupon(name: "Holiday Sale", discount: 20)
+        
+        // Logging
+        print("Shopping cart before applying coupon:")
+        for product in shoppingCart.getProducts() {
+            print("\(product.name) - $\(product.price)")
+        }
+        print("Total price: $\(shoppingCart.totalPrice)")
+        
+        // When
+        shoppingCart.apply(coupon)
+        
+        // Logging
+        print("Shopping cart after applying coupon:")
+        for product in shoppingCart.getProducts() {
+            print("\(product.name) - $\(product.price)")
+        }
+        print("Total price: $\(shoppingCart.totalPrice)")
+        
+        // Then
+        XCTAssertEqual(shoppingCart.totalPrice, 28)
+    }
 }
 
-// --- Running all of our unit tests within the playground ---
+class ShoppingCart {
+    private var products: [Product] = []
 
-// Check Xcode's console for details
-XCTestSuite.default.run()
+    func add(_ product: Product) {
+        products.append(product)
+    }
+
+    func remove(_ product: Product) {
+        if let index = products.firstIndex(of: product) {
+            products.remove(at: index)
+        }
+    }
+
+    var totalPrice: Double {
+        products.reduce(0) { $0 + $1.price }
+    }
+
+    func apply(_ coupon: Coupon) {
+        for index in products.indices {
+            products[index].apply(coupon)
+        }
+    }
+    func getProducts() -> [Product] {
+        return products
+    }
+}
+
+struct Product: Equatable {
+    let name: String
+    var price: Double
+}
+
+struct Coupon: Equatable {
+    let name: String
+    let discount: Double
+}
+// --- Running all of our unit tests within the playground ---
+let testSuite = XCTestSuite.default
+testSuite.run()
